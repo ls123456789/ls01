dcdd2d80d5fefa47aaf577d109801ed6312bafbf
ls123456789 <20141105040ls@iMac-11.local>
2015-11-11 20:36:06 +0800

    指针：第一次提交
    &为取址符，获取变量的地址。＊为间接取值符，通过指针获取指针所指向的存储空间存储的值。数组在使用时，下标从0开始，数组名就是数组的首地址，所以在
    该程序中  a ＝ p  。指针的下标用法与数组的相似，所以在该程序中  p[i] = a[i]  。

--- ls01/main.cpp
+++ ls01/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  ls01
+//  test
 //
 //  Created by 梁爽 on 15/11/11.
 //  Copyright (c) 2015年 梁爽. All rights reserved.
@@ -10,6 +10,15 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0] = 10;
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+    p = a;
+    printf("&a[0] = %x &a[1] = %x &a[2] = %x &a[3] = %x a = %x\n",&a[0],&a[1],&a[2],&a[3],a);
+    printf("p = %x  *p = %d  &p = %x\n",p,*p,&p);
+    printf("p[0] = %d  p[1] = %d",p[0],p[1]);
     return 0;
 }
