2c27030c2fb5127328e0f5096476903136169901
ls123456789 <20141105040ls@iMac-11.local>
2015-11-11 20:44:21 +0800

    因为在程序中  a = p  ，所以在指针变量＋1的时候，可以用数组名＋1来代替。

--- ls01/main.cpp
+++ ls01/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, const char * argv[]) {
     p = a;
     printf("&a[0] = %x &a[1] = %x &a[2] = %x &a[3] = %x a = %x\n",&a[0],&a[1],&a[2],&a[3],a);
     printf("p = %x  *p = %d  &p = %x\n",p,*p,&p);
-    printf("p[0] = %d  p[1] = %d",p[0],p[1]);
+    printf("p[0] = %d  p[1] = %d\n",p[0],p[1]);
+    printf("a[1] = %d\n",*(a+1));
     return 0;
 }
